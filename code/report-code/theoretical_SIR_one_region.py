12732f8e337763ff4b7bc6b648fe8f925df39a9c
frederik wolgast rørbech <frederik.ubuntu.wolgast@gmail.com>
2016-06-17 12:22:16 +0200

    wrote more latex and added code for numerical SIR-one-region solution

---
+++ code/report-code/theoretical_SIR_one_region.py
@@ -0,0 +1,31 @@
+""" Plots the theoretical SIR curve """
+
+import numpy as np
+from scipy import integrate
+import matplotlib.pyplot as plt
+
+N = 100
+beta = 0.1
+gamma = 0.01
+
+
+def solver(Y, t):
+    return [- beta / N * Y[1] * Y[0],
+            beta / N * Y[1] * Y[0] - gamma * Y[1],
+            gamma * Y[1] ]
+
+start_infection = 10
+t = np.arange(0, 365, 0.01)
+asol = integrate.odeint(solver, [N - start_infection,
+                                 start_infection,
+                                 0], t)
+plt.figure(figsize=(12, 8))
+plt.plot(t, asol)
+plt.legend(["Susceptible", "Infected", "Recovered"], loc=7)
+plt.title("Theorical SIR. N={0:d}, Beta={1:.2f}, Gamma={2:.2f}".format(
+    N, beta, gamma
+))
+plt.xlabel("Time")
+plt.ylabel("# of individuals")
+plt.savefig('sir_one_region.pdf', format='pdf',
+            dpi=1000, bbox_inches='tight')
