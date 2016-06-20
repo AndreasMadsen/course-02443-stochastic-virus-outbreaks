5d540ffd40c0283aa62b826057b3ec7c88c83f02
Andreas Madsen <amwebdk@gmail.com>
2016-06-20 15:27:03 +0200

    [report] change coloring one 1-region ode

--- code/report-code/theoretical_SIR_one_region.py
+++ code/report-code/theoretical_SIR_one_region.py
@@ -1,8 +1,14 @@
 """ Plots the theoretical SIR curve """
 
+import sys
+import os.path as path
+
 import numpy as np
 from scipy import integrate
 import matplotlib.pyplot as plt
+
+thisdir = path.dirname(path.realpath(__file__))
+sys.path.append(path.join(thisdir, "../"))
 
 N = 100
 beta = 0.1
@@ -20,12 +26,14 @@ asol = integrate.odeint(solver, [N - start_infection,
                                  start_infection,
                                  0], t)
 plt.figure(figsize=(12, 8))
-plt.plot(t, asol)
+plt.plot(t, asol[:, 0], ls='-', color='g')
+plt.plot(t, asol[:, 1], ls='-', color='r')
+plt.plot(t, asol[:, 2], ls='-', color='b')
 plt.legend(["Susceptible", "Infected", "Recovered"], loc=7)
 plt.title("Theorical SIR. N={0:d}, Beta={1:.2f}, Gamma={2:.2f}".format(
     N, beta, gamma
 ))
 plt.xlabel("Time")
 plt.ylabel("# of individuals")
-plt.savefig('sir_one_region.pdf', format='pdf',
-            dpi=1000, bbox_inches='tight')
+plt.savefig(path.join(thisdir, '../../report/plots/sir_one_region.pdf'),
+            format='pdf', dpi=1000, bbox_inches='tight')
