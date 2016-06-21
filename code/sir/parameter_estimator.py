d78ce9a0514cb82d5b6eb40395944bfddfaf9927
frederik wolgast rørbech <frederik.ubuntu.wolgast@gmail.com>
2016-06-21 14:50:12 +0200

    temp commit of weird variance reduction results

--- code/sir/parameter_estimator.py
+++ code/sir/parameter_estimator.py
@@ -1,3 +1,4 @@
+#temp commit
 
 import math
 import textwrap
@@ -14,6 +15,8 @@ class ParameterEstimator:
             self._max_estimator(sir_iterator)
         elif method == 'regression':
             self._regression_estimator(sir_iterator)
+        elif method == 'implied':
+            self._implied_estimator(sir_iterator)
         else:
             raise NotImplementedError('method %s is not implemented' % method)
 
@@ -51,6 +54,9 @@ class ParameterEstimator:
         self.beta = (k1 - k2) / (1 - survival_rate)
         self.gamma = (k1 * survival_rate - k2) / (1  - survival_rate)
 
+        self.max_infected = -42
+        self.max_infected_i = -42
+
     def _max_estimator(self, sir_iterator):
         # Get initialization constant
         first_sir = next(sir_iterator).total_sir()
@@ -85,7 +91,50 @@ class ParameterEstimator:
         print('max_infected = %d' % max_infected)
         print('max_infected_i = %d' % max_infected_i)
 
+        self.max_infected = max_infected
+        self.max_infected_i = max_infected_i
+
+    # def _implied_estimator(self, sir_iterator):
+    #     """
+    #     estimates beta and gamma directly from total sir
+    #     """
+    #     prev_sir = next(sir_iterator).total_sir()
+    #     self.init_infected = prev_sir.infected
+    #     N = prev_sir.total_pop
+    #     X_beta = []
+    #     y_beta = []
+
+    #     X_gamma = []
+    #     y_gamma = []
+    #     for i, state in enumerate(sir_iterator):
+    #         sir = state.total_sir()
+    #         s_diff = sir.susceptible - prev_sir.susceptible
+    #         #X.append([-np.log(sir.susceptible),
+    #         #          -np.log(sir.infected)])
+    #         X_beta.append([-N / sir.susceptible / sir.infected])
+    #         y_beta.append(s_diff)
+
+    #         r_diff = sir.removed - prev_sir.removed
+
+    #         X_gamma.append([1 / sir.infected])
+    #         y_gamma.append(r_diff)
+
+    #         prev_sir = sir
+
+    #     beta_est = np.linalg.lstsq(X_beta, y_beta)[0]
+    #     gamma_est = np.linalg.lstsq(X_gamma, y_gamma)[0]
+
+    #     print("beta_est {0}".format(beta_est))
+    #     print("gamma_est {0}".format(gamma_est))
+
+    #     self.gamma = gamma_est
+    #     self.beta = beta_est
+    #     self.max_infected = -42
+    #     self.max_infected_i = -42
+    #     self.population = N
+
     def __str__(self):
-        return "Estimator(gamma=%f, beta=%f, N=%d, I=%d)" % (
-            self.gamma, self.beta, self.population, self.init_infected
+        return "Estimator(gamma=%f, beta=%f, N=%d, I=%d, MAX_I=%d, MAX_t=%d)" % (
+            self.gamma, self.beta, self.population, self.init_infected,
+            self.max_infected, self.max_infected_i
         )
