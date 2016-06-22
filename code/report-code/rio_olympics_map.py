31546d329ef67e9fbfed424b01f7fccb35c36ac6
frederik wolgast rørbech <frederik.ubuntu.wolgast@gmail.com>
2016-06-23 00:48:19 +0200

    changed gifs to mpeg to enable tight animation axis

--- code/report-code/rio_olympics_map.py
+++ code/report-code/rio_olympics_map.py
@@ -18,8 +18,8 @@ sim = Simulator(state, transfer_prob=0.005, beta=2, gamma=0.5, verbose=True,)
 sim.add_event(2560, days=18, total_transfer=380e3)
 base_map = WorldMap(resolution="c")
 base_map.animate(sim, frames=len_simulation, max_infected=0.1)
-base_map.ani.save(path.join(this_dir, '../../report/plots/gifs/rio.gif'),
-    writer="imagemagick", fps=3)
+base_map.ani.save(path.join(this_dir, '../../report/plots/gifs/rio.mpg'),
+                  writer="mencoder_file", fps=3, savefig_kwargs={'bbox_inches':'tight'})
 
 
 state = State(regions, routes, verbose=True)
@@ -27,5 +27,5 @@ state.set_outbreak('Rio De Janeiro', 1000)
 sim = Simulator(state, transfer_prob=0.005, beta=2, gamma=0.5, verbose=True)
 base_map = WorldMap(resolution="c")
 base_map.animate(sim, frames=len_simulation, max_infected=0.1)
-base_map.ani.save(path.join(this_dir, '../../report/plots/gifs/no_rio.gif'),
-    writer="imagemagick", fps=3)
+base_map.ani.save(path.join(this_dir, '../../report/plots/gifs/no_rio.mpg'),
+                  writer="mencoder_file", fps=3, savefig_kwargs={'bbox_inches':'tight'})
